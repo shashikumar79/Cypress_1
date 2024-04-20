0b90f600f8fd745bc4477792c4353034980a1fe7
shashikumar79 <shashikumar7979@gmail.com>
2024-04-20 14:55:36 +0530

    first commit

---
+++ cypress/e2e/demonob.cy.js
@@ -0,0 +1,31 @@
+cy.on('uncaught:exception',()=>{
+
+    return false
+});
+describe('demonobcommerce',()=>{
+
+    it('commerce',()=>{
+        
+
+
+cy.visit('https://demo.nopcommerce.com/')
+cy.reload()
+cy.contains('Register').click().should('have.text','Register')
+cy.get('#gender-male').check().should('be.checked')
+cy.get('[id="FirstName"]').type('IHFUg')
+cy.get('[id="LastName"]').type('fpgfpi')
+cy.get('[id="Email"]').type('gfPI@gmail.com')
+cy.get('[id="Company"]').type('FOYPYEGFOHJS')
+cy.get('[type="checkbox"]').check()
+
+cy.get('select[name="DateOfBirthDay"]').select("10")
+cy.get('[name="DateOfBirthMonth"]').select('May')
+cy.get('[name="DateOfBirthYear"]').select('1950')
+
+cy.go('back')
+cy.get('#customerCurrency').select('US Dollar')
+
+
+
+    })
+})
